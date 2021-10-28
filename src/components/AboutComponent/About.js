89c940309366726f2236a2132bcfeaefd14991e3
Josiah Sanchez <josiah.rasanchez@gmail.com>
2021-10-28 15:29:24 -0600

    Finished the aboutme page

--- src/components/AboutComponent/About.js
+++ src/components/AboutComponent/About.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { AboutBody, AboutCountainer, AboutHeader, AboutImage, AboutSubHead, HomeContentContainer } from './AboutElements';
+import { AboutBody, AboutCountainer, AboutHeader, AboutImage, AboutImageContiner, AboutSubHead, AboutContentContainer, AboutContentSpacer, BodyContainer } from './AboutElements';
 import OfMe from '../../assets/PicOfMe.jpg'
 
 export default function About(){
     return (
-    <HomeContentContainer>
-        <AboutImage src={OfMe} alt="Pic of Taylor Larson"/>
-        <AboutCountainer>
-            <AboutHeader>A little More About Me...</AboutHeader>
-            <AboutSubHead>Im Taylor Larson and I'm a Woodworker.</AboutSubHead>
-            <AboutBody>This is the body of my about section...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer neque diam, volutpat vel enim vel, rutrum condimentum enim. Nullam congue, lectus a iaculis gravida, sem odio euismod eros, et vulputate purus diam quis lectus. Donec condimentum nisi et porttitor ullamcorper. Suspendisse eleifend elit ornare augue vulputate, in pulvinar sem sodales. Phasellus egestas ex sed sodales malesuada. Phasellus ut sollicitudin dui. Praesent libero dolor, aliquet et blandit sollicitudin, lacinia ut ligula. Proin lacinia nunc a mauris commodo efficitur eget vitae nisl. Fusce lobortis, dolor at sagittis semper, augue nibh iaculis turpis, a hendrerit magna enim quis lacus. Etiam pretium enim in metus rutrum malesuada. Suspendisse ultrices erat mollis turpis sollicitudin porta. Maecenas est lectus, fermentum vel tristique eu, scelerisque nec diam. Nunc a quam in tellus vulputate eleifend. Etiam mattis, nulla id iaculis posuere, lacus tellus faucibus enim, quis interdum enim leo sit amet velit. Fusce vitae magna et eros placeraLorem ipsum dolor sit amet, consectetur adipiscing elit. Integer neque diam, volutpat vel enim vel, rutrum condimentum enim. Nullam congue, lectus a iaculis gravida, sem odio euismod eros, et vulputate purus diam quis lectus. Donec condimentum nisi et porttitor ullamcorper. Suspendisse eleifend elit ornare augue vulputate, in pulvinar sem sodales. Phasellus egestas ex sed sodales malesuada. Phasellus ut sollicitudin dui. Praesent libero t laoreet.</AboutBody>
-        </AboutCountainer>
-    </HomeContentContainer>
+    <AboutContentContainer>
+            <AboutImageContiner>
+                <AboutImage src={OfMe} alt="Pic of Taylor Larson"/>
+            </AboutImageContiner>
+            
+            <AboutCountainer>
+                <BodyContainer>
+                    <AboutHeader>A little More About Me...</AboutHeader>
+                    <AboutSubHead>Woodworker by Day & Artist by Night</AboutSubHead>
+                
+                    <AboutBody>In my spare time I am an artist. I enjoy oil painting and sketching with pastels. 
+                        Preferably I paint photorealistic portraits or landscapes, but I enjoy most kinds of art as well. 
+                        I like looking at the fine details that make a piece what it is, chaos on a smaller level creates harmony on a larger level!
+                    </AboutBody>
+                </BodyContainer>
+            </AboutCountainer>
+    </AboutContentContainer>
         )
 }
